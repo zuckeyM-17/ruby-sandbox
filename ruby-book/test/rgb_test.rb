0bc8a88f2506c7ec3ddb2fb722f877f0df2d5c62
松村和輝 <k.matsumura0314@gmail.com>
2018-03-20 12:45:46 +0900

    complete impl to_ints

--- ruby-book/test/rgb_test.rb
+++ ruby-book/test/rgb_test.rb
@@ -10,6 +10,7 @@ class RgbTest < Minitest::Test
 
   def test_to_ints
     assert_equal [0, 0, 0], to_ints('#000000')
-    # assert_equal [255, 255, 255], to_ints('#ffffff')
+    assert_equal [4, 60, 120], to_ints('#043c78')
+    assert_equal [255, 255, 255], to_ints('#ffffff')
   end
 end
