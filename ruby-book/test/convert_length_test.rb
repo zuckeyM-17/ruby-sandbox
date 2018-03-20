8a66432a7d3a67868028d2369ef9ac83c2028b0d
松村和輝 <k.matsumura0314@gmail.com>
2018-03-20 20:58:29 +0900

    first impl convert_length

---
+++ ruby-book/test/convert_length_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require_relative '../lib/convert_length'
+
+class ConvertLengthTest < Minitest::Test
+  def test_convert_length
+    assert_equal 39.37, convert_length(1, 'm', 'in')
+  end
+end
