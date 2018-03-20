b076bbc73dd62a59bfc6bd5f50619e1bceb06af6
松村和輝 <k.matsumura0314@gmail.com>
2018-03-20 11:35:04 +0900

    separate test

--- ruby-book/lib/fizz_buzz.rb
+++ ruby-book/lib/fizz_buzz.rb
@@ -1,3 +1,4 @@
+
 def fizz_buzz n
   if n % 15 == 0
     'FizzBuzz'
@@ -7,19 +8,5 @@ def fizz_buzz n
     'Buzz'
   else
     n.to_s
-  end
-end
-
-require 'minitest/autorun'
-
-class FizzBuzzTest < Minitest::Test
-  def test_fizz_buzz
-    assert_equal '1', fizz_buzz(1)
-    assert_equal '2', fizz_buzz(2)
-    assert_equal 'Fizz', fizz_buzz(3)
-    assert_equal '4', fizz_buzz(4)
-    assert_equal 'Buzz', fizz_buzz(5)
-    assert_equal 'Fizz', fizz_buzz(6)
-    assert_equal 'FizzBuzz', fizz_buzz(15)
   end
 end
