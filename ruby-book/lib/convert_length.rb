81195e5c4ee19af20e562f49392dae7f18e25b7e
松村和輝 <k.matsumura0314@gmail.com>
2018-03-20 21:09:58 +0900

    change convert_length args from string to simbol

--- ruby-book/lib/convert_length.rb
+++ ruby-book/lib/convert_length.rb
@@ -1,8 +1,8 @@
 def convert_length(length, unit_from, unit_to)
   units = {
-    'm' => 1.0,
-    'ft' => 3.28,
-    'in' => 39.37,
+    m: 1.0,
+    ft: 3.28,
+    in: 39.37,
   }
   (length / units[unit_from] * units[unit_to]).round(2)
 end
