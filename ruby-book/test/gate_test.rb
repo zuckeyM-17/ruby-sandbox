48719195f78dab394e85d6c0834f1c5fec6a47bd
松村和輝 <k.matsumura0314@gmail.com>
2018-03-21 10:34:07 +0900

    complete impl gate

--- ruby-book/test/gate_test.rb
+++ ruby-book/test/gate_test.rb
@@ -1,8 +1,23 @@
 require 'minitest/autorun'
 require_relative '../lib/gate.rb'
+require_relative '../lib/ticket.rb'
 
 class GateTest < Minitest::Test
   def test_gate
-    assert Gate.new
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    assert juso.exit(ticket)
+  end
+
+  def test_umeda_to_mikuni_when_fare_is_not_enough
+    umeda = Gate.new(:umeda)
+    mikuni = Gate.new(:mikuni)
+
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    refute mikuni.exit(ticket)
   end
 end
