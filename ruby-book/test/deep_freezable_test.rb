4c43b5f792993b743cd204199f36ae74a690761e
松村和輝 <k.matsumura0314@gmail.com>
2018-03-22 15:05:53 +0900

    deep freeze hash

--- ruby-book/test/deep_freezable_test.rb
+++ ruby-book/test/deep_freezable_test.rb
@@ -8,4 +8,10 @@ class DeepFreezableTest < Minitest::Test
     assert Team::COUNTRIES.frozen?
     assert Team::COUNTRIES.all? { |country| country.frozen? }
   end
+
+  def test_deep_freeze_to_hash
+    assert_equal({ 'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee' }, Bank::CURRENCIES)
+    assert Bank::CURRENCIES.frozen?
+    assert Bank::CURRENCIES.all? { |key, value| key.frozen? && value.frozen? }
+  end
 end
