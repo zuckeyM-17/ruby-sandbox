8b9b8bc0df15e76d5627a8805a1e3be29ff61396
松村和輝 <k.matsumura0314@gmail.com>
2018-03-23 18:38:59 +0900

    first impl regexp_checker
    ex)
    $ ruby ruby-book/lib/regexp_checker.rb
    Text?: 123-456-789
    Pattern?: [1-6]+
    Matched: 123, 456

---
+++ ruby-book/lib/regexp_checker.rb
@@ -0,0 +1,16 @@
+print 'Text?: '
+
+text = gets.chomp
+
+print 'Pattern?: '
+
+pattern = gets.chomp
+
+regexp = Regexp.new(pattern)
+matches = text.scan(regexp)
+
+if matches.size > 0
+  puts "Matched: #{matches.join(', ')}"
+else
+  puts "Nothing matched."
+end
